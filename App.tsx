2791a33cf81cbe3ac5b5b9db67d6a1fa9f6bc208
3kingz <victorkenneth95@gmail.com>
2021-06-12 14:29:55 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Mobile Calculator</Text>
+    </View>
+  );
+};
+
+export default App;
