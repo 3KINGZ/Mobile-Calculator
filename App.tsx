127e4e62e82ebfa87f85fc217917d026deee8ca2
3kingz <victorkenneth95@gmail.com>
2021-06-12 18:16:30 +0100

    ui component setup

--- App.tsx
+++ App.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import uuid from "react-native-uuid";
+import { Button } from "./src/component";
+
+import { scale } from "react-native-size-matters";
+
+import { COLORS } from "./src/styles";
 
 const funcType = {
   FUNC: "func",
@@ -10,110 +21,144 @@ const funcType = {
 const calcBTNS = [
   {
     id: uuid.v4(),
-    type: funcType.FUNC,
-    emit: "clear",
+    type: "clear",
+    emit: "C",
+    value: "C",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "(",
+    value: "(",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: ")",
+    value: ")",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
-    emit: ")",
-  },
-  {
-    id: uuid.v4(),
-    type: funcType.FUNC,
+    type: "delete",
     emit: "delete",
+    value: "delete",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "7",
+    value: "7",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "8",
+    value: "8",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "9",
+    value: "9",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "÷",
+    value: "/",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "4",
+    value: "4",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "5",
+    value: "5",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "6",
+    value: "6",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "x",
+    value: "*",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "1",
+    value: "1",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "2",
+    value: "2",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "3",
+    value: "3",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "-",
+    value: "-",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: ".",
+    value: ".",
   },
   {
     id: uuid.v4(),
-    type: funcType.EMIT,
+    type: "add",
     emit: "0",
+    value: "0",
   },
   {
     id: uuid.v4(),
-    type: funcType.FUNC,
-    emit: "percent",
+    type: "add",
+    emit: "%",
+    value: "%",
   },
   {
     id: uuid.v4(),
-    type: funcType.FUNC,
+    type: "add",
     emit: "+",
+    value: "+",
   },
 ];
+
+const WIDTH = Dimensions.get("window").width;
+
+const CalcButton = ({ onPress, value }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        alignItems: "center",
+        width: WIDTH / 4,
+        justifyContent: "center",
+        padding: "6%",
+      }}>
+      <Text style={{ color: COLORS.primary, fontSize: scale(15) }}>
+        {value}
+      </Text>
+    </TouchableOpacity>
+  );
+};
 
 const App = () => {
   const [calc, setCalc] = useState<string>("");
@@ -121,20 +166,76 @@ const App = () => {
 
   const operators = ["+", "-", "÷", "x"];
 
-  const calcFUNCS = {
-    clear: setCalc(""),
-    delete: setCalc(calc.slice(0, -1)),
+  const calcFUNCS: { [key: string]: any } = {
+    clear: () => setCalc(""),
+    delete: () => setCalc(calc.slice(0, -1)),
     add: (val: string) => {
-      if (!operators.includes(calc[calc.length - 1])) {
-        setCalc(val);
+      if (
+        operators.includes(val) &&
+        operators.includes(calc[calc.length - 1])
+      ) {
+        return;
+      } else {
+        setCalc(calc + val);
       }
     },
-    sum: setResult(eval(calc)),
+    sum: () => setResult(eval(calc)),
   };
 
   return (
-    <View>
-      <Text>Mobile Calculator</Text>
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          padding: "7%",
+          borderBottomWidth: 1,
+          borderColor: COLORS.lightGrey,
+        }}>
+        <Text style={{ textAlign: "right", color: COLORS.primary }}>
+          {calc}
+        </Text>
+      </View>
+      <View
+        style={{
+          padding: "5%",
+          borderBottomWidth: 1,
+          borderColor: COLORS.lightGrey,
+        }}>
+        <Text
+          style={{
+            textAlign: "right",
+            color: COLORS.primary,
+            fontSize: scale(24),
+          }}>
+          {result}
+        </Text>
+      </View>
+      <FlatList
+        data={calcBTNS}
+        numColumns={4}
+        style={{ flex: 1 }}
+        renderItem={({ item }) => (
+          <CalcButton
+            onPress={() => calcFUNCS[item.type](item.value)}
+            value={item.emit}
+          />
+        )}
+      />
+      <Button
+        onPress={calcFUNCS.sum}
+        title="="
+        size="md"
+        style={{
+          containerStyle: {
+            alignSelf: "center",
+            borderRadius: 0,
+            // position: "absolute",
+            // bottom: 0,
+          },
+          textStyle: {
+            fontSize: scale(30),
+          },
+        }}
+      />
     </View>
   );
 };
---
+++ src/component/Button.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Text,
+  GestureResponderEvent,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+
+import { COLORS } from "../styles";
+
+interface IButton {
+  title: string;
+  onPress?:
+    | (((event: GestureResponderEvent) => void) & (() => void))
+    | undefined;
+  size?: "xl" | "md" | "s";
+  style?: { containerStyle?: object; textStyle?: object };
+}
+
+export const Button = ({
+  title,
+  onPress,
+  size,
+  style = { containerStyle: {}, textStyle: {} },
+}: IButton) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.container, styles[size || "s"], style.containerStyle]}>
+      <Text style={[styles.text, style.textStyle]}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    margin: 10,
+    width: "100%",
+  },
+  text: {
+    color: "white",
+    textAlign: "center",
+  },
+  xl: {
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+  },
+  md: {
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  s: {
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+  },
+});
